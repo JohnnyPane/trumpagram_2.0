ebde693e5926a74e1fed4034b3636940a3770afa
JohnnyP <wabanichi@gmail.com>
2019-08-09 15:37:55 -0500

    set up AWS speaker-mp3 upload

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -3,10 +3,12 @@ class Speaker < ApplicationRecord
 
 	has_many :audio_clips,
 		primary_key: :id,
-		foreign_key: :speaker_id
+		foreign_key: :speaker_id,
 		class_name: :AudioClip
 
 	has_many :transcripts, through: :audio_clips
 
 	has_many :words, through: :transcripts
+
+	has_many_attached :mp3s
 end
