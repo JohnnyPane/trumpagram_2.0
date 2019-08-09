be3c67f7ab30805f0293f1dbab08ca4601442b48
JohnnyP <wabanichi@gmail.com>
2019-08-09 13:11:56 -0500

    create app and root components

--- frontend/reducers/speakers_reducer.js
+++ frontend/reducers/speakers_reducer.js
@@ -1,9 +1,32 @@
+import merge from 'lodash/merge';
+
 import { RECEIVE_SPEAKERS,
 				 RECEIVE_SPEAKER,
 				 REMOVE_SPEAKER } from '../actions/speaker_actions';
 
-const speakersReducer = (state = {}, action) => {
+
+const initialState = {
+	1: {
+		id: 1,
+		name: 'Donald Trump'
+	},
+	2: {
+		id: 2,
+		name: 'Bernie Sanders'
+	}
+}
+
+const speakersReducer = (state = initialState, action) => {
 	switch(action.type) {
+		case RECEIVE_SPEAKERS:
+			return action.speakers
+		case RECEIVE_SPEAKER:
+			const newSpeaker = { [action.speaker.id]: action.speaker };
+			return merge({}, state, newSpeaker);
+		case REMOVE_SPEAKER:
+			nextState = merge({}, state);
+			delete nextState[action.speaker.id];
+			return nextState;
 		default: 
 			return state;
 	}
--- frontend/trumpgram.jsx
+++ frontend/trumpgram.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store'
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
 	const store = configureStore();
-	window.store = store;
+	window.getState = store.getState;
+	window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Trump Mania!</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
